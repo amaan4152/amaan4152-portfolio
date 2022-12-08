d8a35eb54623f2fccbd3b5ff4ab23d02e2ee6ff1
amaan4152 <amaan.rahman.usa@gmail.com>
2022-12-08 06:45:58 -0500

    revamped portfolio - READY

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,25 @@ import Head from 'next/head'
 import Navbar from "./Navbar"
 import { useRouter } from 'next/router'
 import {
+    Box,
     Center,
     Container,
     Divider,
     VStack,
     Image,
     HStack,
+    Heading,
     Icon,
-    Link
+    Link,
+    TableContainer,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Td,
+    Th,
+    Text,
+    Flex
 } from '@chakra-ui/react'
 import { GoMarkGithub, GoMail } from 'react-icons/go'
 import { FaLinkedin } from 'react-icons/fa'
@@ -31,6 +42,7 @@ export default function Layout({ children, ...extraMeta }) {
         <div>
             <Head>
                 <title>{meta.title}</title>
+                <link rel="shortcut icon" href="githubLogo.png" />
                 <meta name="og:title" content={meta.title} />
                 <meta name="og:name" content={meta.name} />
                 <meta name="og:github" content={meta.github} />
@@ -71,7 +83,11 @@ export default function Layout({ children, ...extraMeta }) {
                             fontSize="16px"
                             textAlign="center"
                         >
-                            The Cooper Union M.Engg & EE '23
+                            <Text>
+                                The Cooper Union
+                                M.Engg. & B.E. in EE '23
+                            </Text>
+
                         </Container>
                         <HStack>
                             <Container pr={3}>
@@ -100,12 +116,76 @@ export default function Layout({ children, ...extraMeta }) {
                                 </Link>
                             </Container>
                         </HStack>
-                        <Divider pt={5} />
+                        <Divider pt={5} borderColor="black" />
                         <Container
+                            maxW="container.lg"
                             pt={5}
                         >
                             {children}
                         </Container>
+                        <Divider pt={5} borderColor="black" />
+                        <Heading
+                            fontSize="30px"
+                            fontFamily="consolas"
+                        >
+                            ls skills/
+                        </Heading>
+                        <Container pb={5}>
+                            <TableContainer>
+                                <Table variant='simple'>
+                                    <Thead>
+                                        <Tr>
+                                            <Th>
+                                                <HStack>
+                                                    <Text>Software</Text>
+                                                    <Image w="10%" src="programming.png" />
+                                                </HStack>
+                                            </Th>
+                                            <Th>
+                                                <HStack>
+                                                    <Text>Hardware</Text>
+                                                    <Image w="10%" src="hw_icon.png" />
+                                                </HStack>
+                                            </Th>
+                                        </Tr>
+                                    </Thead>
+                                    <Tbody>
+                                        <Tr>
+                                            <Td>Python</Td>
+                                            <Td>Cadence Virtuoso</Td>
+                                        </Tr>
+                                        <Tr>
+                                            <Td>MATLAB</Td>
+                                            <Td>Synopsis Design Compiler</Td>
+                                        </Tr>
+                                        <Tr>
+                                            <Td>Verilog/SystemVerilog HDL</Td>
+                                            <Td>Synopsys IC Compiler II</Td>
+                                        </Tr>
+                                        <Tr>
+                                            <Td>C++</Td>
+                                            <Td>Vivado Design Suite</Td>
+                                        </Tr>
+                                        <Tr>
+                                            <Td>C</Td>
+                                            <Td>LTSpice</Td>
+                                        </Tr>
+                                        <Tr>
+                                            <Td>x86 Assembly</Td>
+                                            <Td>Zynq 7000 ZedBoard</Td>
+                                        </Tr>
+                                        <Tr>
+                                            <Td>Bash & Tcl</Td>
+                                            <Td>UNIX/Linux Systems</Td>
+                                        </Tr>
+                                    </Tbody>
+                                </Table>
+                            </TableContainer>
+                        </Container>
+                        <Divider pb={3} />
+                        <Box align="center" opacity={0.4} fontSize="sm" pb={3}>
+                            &copy; {new Date().getFullYear()} Amaan Rahman. All Rights Reserved.
+                        </Box>
                     </VStack>
                 </Center>
             </main>
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,264 @@
 import Layout from "../components/Layout";
-import { Box, Container, Heading, Link, Wrap } from "@chakra-ui/react"
+import {
+  Box,
+  Card,
+  CardBody,
+  Container,
+  Heading,
+  Image,
+  Link,
+  HStack,
+  Stack,
+  VStack,
+  Wrap,
+  CardHeader,
+  UnorderedList,
+  ListItem,
+  Text,
+  LinkBox,
+  LinkOverlay,
+  Center
+} from "@chakra-ui/react"
 export default function Home() {
   return (
     <Layout>
-      <Heading
-        fontSize="30px"
-        fontFamily="consolas"
-        pb={2}
-      >
-        whoami
-      </Heading>
-      Undergraduate senior Electrical Engineering student at The Cooper Union pursuing combined 4-year Master's of Engineering and Bachelor's of Engineering. Currently doing my M.Engg. thesis on designing a low-cost 3D human pose estimation algorithm motivated from Google's <Link color="teal.300" href="https://google.github.io/mediapipe/solutions/pose.html">Blazepose</Link>. Invested in researching ASIC architectures for accelerating deep neural networks (DNN), espceially in computer vision. Moreover, interested in designing low-power DNN algorithms while maintaining competitive accuracy against state-of-the-art.
+      <Center>
+        <VStack>
+          <Heading
+            fontSize="30px"
+            fontFamily="consolas"
+            pb={2}
+          >
+            whoami
+          </Heading>
+          <Text>
+            Undergraduate senior Electrical Engineering student at The Cooper Union pursuing combined 4-year Master's of Engineering and Bachelor's of Engineering. Currently doing my M.Engg. thesis on designing a low-cost 3D human pose estimation algorithm motivated from Google's <Link color="teal.300" href="https://google.github.io/mediapipe/solutions/pose.html">Blazepose</Link>. Invested in researching ASIC architectures for accelerating deep neural networks (DNN), espceially in computer vision. Moreover, interested in designing low-power DNN algorithms while maintaining competitive accuracy against state-of-the-art.
+          </Text>
 
-      <Heading
-        fontSize="30px"
-        fontFamily="consolas"
-        pt={5}
-        pb={2}
-      >
-        ls projects/
-      </Heading>
-      <Wrap>
-        <Box
-          fontSize="50px"
-          fontFamily="consolas"
-          bg='tomato'
-          w='100%'
-        >
-          50
-        </Box>
-      </Wrap>
+
+          <Heading
+            fontSize="30px"
+            fontFamily="consolas"
+            pt={5}
+            pb={2}
+          >
+            ll projects/
+          </Heading>
+
+          <HStack>
+            <VStack>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="nist_research.jpg"
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/3DPoseEvaluator" target="_blank" rel="noopener noreferrer">
+                          3D Pose Evaluator Tool
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        3D human pose estimation evaluator tool composed of 3 algorithms: GAST-Net, VIBE, and Blazepose. Algorithms are evaluated on Optical Tracking System (OTS) data and metrics utilized are MPJPE and PJD. The tool is pipelined to later be utilized for exoskeleton kinematic evaluations.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="https://pimages.toolbox.com/wp-content/uploads/2022/05/18113202/The-Architecture-of-a-Neural-Network.png"
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/ECE472-DeepLearning" target="_blank" rel="noopener noreferrer">
+                          Deep Learning Projects
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        Developed multilayer perceptrons (MLP), Convolutional Neural Networks (CNN), residual networks, and transformers with Tensorflow.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="EM_algo.png"
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/ECE474-BayesianML" target="_blank" rel="noopener noreferrer">
+                          Bayesian Machine Learning Projects
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        Various projects implemented Bayesion machine learning algorithms and techniques such as kernel methods, Expextation-Maximization, Monte Carlo Markov Chain.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="https://www.freecodecamp.org/news/content/images/2020/09/image-21.png"
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/ECE365-DSA_II" target="_blank" rel="noopener noreferrer">
+                          Data Structures & Algorithms II Projects
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        Implemented various data structures in C++ such as hash tables, heaps, Djikstra's algorithm, and dynamic programming.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+            </VStack>
+
+            <VStack>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="final_layout.png"
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/DVLSI-8bit-counter" target="_blank" rel="noopener noreferrer">
+                          8-bit Counter
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        Developed 8-bit counter with Verilog. Synthesized counter with DC Compiler; designed floorplan and placement, clock, routing optimizations with IC Compiler II; passed DRC and LVS checks.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="vit_model.png"
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/ViT-Probing" target="_blank" rel="noopener noreferrer">
+                          Understanding Vision Transformers with Probing
+                          Techniques
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        Proposed a method of analyzing a vision transformer
+                        through probing techniques to gauge the classification performance of each layer
+                        in the ViT except the classification head. Also performed a comparison study
+                        between a ViT and a naive adaptation of a convolution model to determine the
+                        significance of encoders and patch encoding.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="RepresentativeImage.jpg"
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/Pose-Estimation-Applications" target="_blank" rel="noopener noreferrer">
+                          3D Human Pose Estimation on Computer Animations
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        Utilized 2D human pose estimation algorithm, <Link color="teal.300" href="https://github.com/CMU-Perceptual-Computing-Lab/openpose">Openpose</Link>, to triangulate 3D positional information with stereophotogrammatry in real-time. Applied research to computer animations to work on low-cost alternative to body tracking using 2 RGB cameras.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+              <LinkBox as='article' maxW='sm' p='5'>
+                <Card
+                  w="100%"
+                  h="100%"
+                  shadow="xl"
+                  overflow='hidden'
+                >
+                  <Image
+                    src="https://media.istockphoto.com/id/1216376755/vector/compiler-concept-icon-software-development-machine-language-programming-data-filtering.jpg?s=612x612&w=0&k=20&c=MbSJrIayiUkVlYlD71StWsBayeOny8eKGAYncHHc7rQ="
+                    pt="5"
+                  />
+                  <Stack>
+                    <CardBody bg="gray.300" fontFamily="consolas">
+                      <Heading size='md' fontFamily="consolas">
+                        <LinkOverlay href="https://github.com/amaan4152/ECE466-Compilers" target="_blank" rel="noopener noreferrer">
+                          C Compiler
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="16px">
+                        Developed C compiler from scratch. Composed of 5 stages: Lexical Analyzer, Syntax Parser, Semantic Analyzer, Intermediate Code Generator, Target Code Generator.
+                      </Text>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </LinkBox>
+            </VStack>
+          </HStack>
+        </VStack>
+      </Center>
     </Layout>
   )
 }
